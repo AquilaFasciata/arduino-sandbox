4cbbac950413c06112f54ce897ec6ce88fc9039b
Zach <zachisaunders12@gmail.com>
2024-05-04 18:28:48 -0400

    Added defines for the pins on the LCD

--- led.c
+++ led.c
@@ -4,15 +4,23 @@
 // Reg Sel  Pin 12
 // Enable   Pin 11
 // Data 4-7 Pin 5-2
+
+#define lcdE        PORTB3
+#define lcdRS       PORTB4
+#define lcdD4       PORTD5
+#define lcdD5       PORTD4
+#define lcdD6       PORTD3
+#define lcdD7       PORTD3
+
+#define DIRlcdE     DDRB3
+#define DIRlcdRS    DDRB4
+#define DIRlcdD4    DDRD5
+#define DIRlcdD5    DDRD4
+#define DIRlcdD6    DDRD3
+#define DIRlcdD7    DDRD3
+
+
 int main(void) {
-  // Enable pins to output mode
-  DDRB |= (1 << DDB4);
-  DDRB |= (1 << DDB3);
-  
-  DDRD |= (1 << DDD5);
-  DDRD |= (1 << DDD4);
-  DDRD |= (1 << DDD3);
-  DDRD |= (1 << DDD2);
 
 
 
