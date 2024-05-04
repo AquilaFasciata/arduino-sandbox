93bee8f214af8e2ad15bd47e59b94c52e884d727
Zach <zachisaunders12@gmail.com>
2024-05-04 18:50:23 -0400

    Added RW pin

--- led.c
+++ led.c
@@ -2,10 +2,12 @@
 #include <util/delay.h>
 
 // Reg Sel  Pin 12
+// RW				Pin 0
 // Enable   Pin 11
 // Data 4-7 Pin 5-2
 
 #define lcdE        PORTB3
+#define lcdRW				PORTD0
 #define lcdRS       PORTB4
 #define lcdD4       PORTD5
 #define lcdD5       PORTD4
@@ -13,6 +15,7 @@
 #define lcdD7       PORTD3
 
 #define DIRlcdE     DDRB3
+#define DIRlcdRW		DDRD0
 #define DIRlcdRS    DDRB4
 #define DIRlcdD4    DDRD5
 #define DIRlcdD5    DDRD4
