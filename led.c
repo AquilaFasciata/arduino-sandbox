60bcd260f20549f94ec94ed5a5b35859f9264b57
Zach <zachisaunders12@gmail.com>
2024-05-04 20:47:27 -0400

    More pin defines, some init setup

--- led.c
+++ led.c
@@ -6,26 +6,41 @@
 // Enable   Pin 11
 // Data 4-7 Pin 5-2
 
-#define lcdE        PORTB3
-#define lcdRW				PORTD0
-#define lcdRS       PORTB4
-#define lcdD4       PORTD5
-#define lcdD5       PORTD4
-#define lcdD6       PORTD3
-#define lcdD7       PORTD3
+#define PINlcdE       PORTB3
+#define PINlcdRW			PORTD0
+#define PINlcdRS      PORTB4
+#define PINlcdD4      PORTD5
+#define PINlcdD5      PORTD4
+#define PINlcdD6      PORTD3
+#define PINlcdD7      PORTD3
 
-#define DIRlcdE     DDRB
-#define DIRlcdRW		DDRD
-#define DIRlcdRS    DDRB
-#define DIRlcdD4    DDRD
-#define DIRlcdD5    DDRD
-#define DIRlcdD6    DDRD
-#define DIRlcdD7    DDRD
+#define DIRlcdE 			DDRB
+#define DIRlcdRW			DDRD
+#define DIRlcdRS    	DDRB
+#define DIRlcdD4    	DDRD
+#define DIRlcdD5    	DDRD
+#define DIRlcdD6    	DDRD
+#define DIRlcdD7    	DDRD
 
+#define PORTlcdE    	PORTB
+#define PORTlcdRW			PORTD
+#define PORTlcdRS   	PORTB
+#define PORTlcdD4   	PORTD
+#define PORTlcdD5   	PORTD
+#define PORTlcdD6   	PORTD
+#define PORTlcdD7   	PORTD
 
 int main(void) {
 
-	DIRlcdE |= _BV(lcdE);
+	DIRlcdE		|= _BV(PINlcdE);
+	DIRlcdRW	|= _BV(PINlcdRW);
+	DIRlcdRS	|= _BV(PINlcdRS);
+	DIRlcdD4	|= _BV(PINlcdD4);
+	DIRlcdD5	|= _BV(PINlcdD5);
+	DIRlcdD6	|= _BV(PINlcdD6);
+	DIRlcdD7	|= _BV(PINlcdD7);
+
+	PORTlcdRW	|= _BV(PINlcdRW);
 
 	while(1) {
 
