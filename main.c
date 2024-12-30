ebe38948d119260687ce2d5eb0905d2e48399f0b
Zach Saunders <zachisaunders12@gmail.com>
2024-12-30 13:40:56 -0500

    Adding serial for debugging

--- main.c
+++ main.c
@@ -1,15 +1,30 @@
 #include "func.h"
 #include "pindefines.h"
 #include <avr/io.h>
+#include <avr/sfr_defs.h>
 #include <util/delay.h>
 
-#define BLINK_DELAY_MS 500
+#define BLINK_DELAY_MS 1000
 
 // Reg Sel  Pin 12
 // RW				Pin 8
 // Enable   Pin 11
 // Data 4-7 Pin 5-2
 // RW H Read L Write
+
+#define FOSC 16000000UL
+#define BAUD 115200
+#define MYUBRR FOSC / (16 * BAUD) - 1
+
+void usart_init(unsigned int ubrr) {
+  UCSR0C = 0;
+  UBRR0H = (unsigned char)(ubrr >> 8);
+  UBRR0L = (unsigned char)ubrr;
+  // Enable
+  UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+  // 8 bit char; 2 bit stop
+  UCSR0C = (1 << USBS0) | (3 << UCSZ00);
+}
 
 int main() {
   DDRB |= _BV(PORTB5); // Builtin LED Direction Register
@@ -24,7 +39,7 @@ int main() {
   // Set RW to read
   PORTlcdRW |= _BV(PINlcdRW);
 
-  initLcd();
+  // initLcd();
   lcdDataWrite(0b01000001, RAM);
   // busyWait();
   _delay_us(5);
@@ -36,6 +51,7 @@ int main() {
   _delay_us(5);
 
   DDRB |= 0b100000; // PORTB5
+  usart_init(MYUBRR);
   while (1) {
     // turn LED on
     PORTB |= 0b100000; // PORTB5
@@ -44,5 +60,12 @@ int main() {
     // turn LED off
     PORTB &= ~0b100000; // PORTB5
     _delay_ms(BLINK_DELAY_MS);
+
+    if ((UCSR0A & (1 << RXC0))) {
+      unsigned char data = UDR0;
+      while (!(UCSR0A & (1 << UDRE0))) {
+        UDR0 = data;
+      }
+    }
   }
 }
---
+++ old.jsp
@@ -0,0 +1,32 @@
+[
+  {
+    "arguments": [
+      "/usr/bin/avr-gcc",
+      "-Os",
+      "-DF_CPU=16000000UL",
+      "-mmcu=atmega328p",
+      "-c",
+      "-o",
+      "main.o",
+      "main.c"
+    ],
+    "directory": "/home/zach/dev/arduino-sandbox",
+    "file": "/home/zach/dev/arduino-sandbox/main.c",
+    "output": "/home/zach/dev/arduino-sandbox/main.o"
+  },
+  {
+    "arguments": [
+      "/usr/bin/avr-gcc",
+      "-Os",
+      "-DF_CPU=16000000UL",
+      "-mmcu=atmega328p",
+      "-c",
+      "-o",
+      "func.o",
+      "func.c"
+    ],
+    "directory": "/home/zach/dev/arduino-sandbox",
+    "file": "/home/zach/dev/arduino-sandbox/func.c",
+    "output": "/home/zach/dev/arduino-sandbox/func.o"
+  }
+]
